421869a43011ab7776a28677a6dab24f0ca292d8
Sev <82528808+iarum@users.noreply.github.com>
2023-12-07 16:57:59 +0400

    b

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,23 @@
 let q = 0;
+const giorgi = $('#giorgi');
 $(document).on('click', function () {
     q++;
 
     audio = new Audio(`assets/sound/punch.mp3`);
     audio.play();
 
+    giorgi.css('filter', 'hue-rotate(' + (q * 2) + 'deg)');
+
     if (q % 2 === 0) {
         $('.right').css('top', '50%');
         $('.left').css('top', '80%');
+
+        giorgi.css('transform', 'rotate(4deg)');
+
     } else {
         $('.left').css('top', '50%');
         $('.right').css('top', '80%');
+        giorgi.css('transform', 'rotate(-4deg)');
     }
 
 
